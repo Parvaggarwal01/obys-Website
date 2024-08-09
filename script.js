0f31f69b156b8633944c78e2165173e0d30e2acd
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-08-10 00:08:32 +0530

    Adding Favicon

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-var h5timer = document.querySelector("#line1-part1 h5");
-var grow = 0;
+function loadingAnimation() {
+  var h5timer = document.querySelector("#line1-part1 h5");
+  var grow = 0;
 
-var tl = gsap.timeline();
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
+  tl.from(".line h1", {
     y: +150,
     stagger: 0.25,
     opacity: 0,
     duration: 0.6,
     delay: 0.5,
-})
+  });
 
-tl.from("#line1-part1", {
+  tl.from("#line1-part1", {
     opacity: 0,
-    onStart: function() {
-        setInterval(function(){
-            if(grow<100){
-                h5timer.innerHTML = grow++;
-            }
-            else{
-                h5timer.innerHTML = grow;
-            }
-        }, 38)
-    }
-})
+    onStart: function () {
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 38);
+    },
+  });
 
-tl.to(".line h2", {
+  tl.to(".line h2", {
     animationName: "anime",
     opacity: 1,
-})
+  });
 
-tl.to("#loader", {
+  tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
-    delay: 4
-})
+    delay: 4,
+  });
 
-tl.from("#page1", {
+  tl.from("#page1", {
     delay: 0.2,
     y: 1600,
     opacity: 0,
     duration: 0.5,
-})
+  });
 
-tl.to("#loader", {
+  tl.to("#loader", {
     display: "none",
-})
+  });
+}
+
+
+loadingAnimation();
